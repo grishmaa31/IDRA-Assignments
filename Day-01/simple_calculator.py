a133c64482e721a870dd44d6b6f51a6d387f0dc3
grishmaa31 <grishmamadhvani@gmail.com>
2026-08-04 10:03:10 +0530

    Add Day 1 Simple Calculator Assignment
    Implement a simple calculator that performs addition, subtraction, multiplication, division, floor division, modulus, and exponentiation.

---
+++ Day-01/simple_calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+floor = num1 // num2
+mod = num1 % num2
+exp = num1 ** num2
+
+print("\nResults")
+print(f"Addition = {add}")
+print(f"Subtraction = {sub}")
+print(f"Multiplication = {mul}")
+print(f"Division = {div}")
+print(f"Floor Division = {floor}")
+print(f"Modulus = {mod}")
+print(f"Exponentiation = {exp}")
